a355cf41a79ca6594c5cc8962bf9db10e46ed235
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2022-01-13 16:56:27 -0300

    Create a Design Pattern MVC

--- Calculator-Advanced-Swift-iOS13/Calculator/Controller/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13/Calculator/Controller/ViewController.swift
@@ -34,14 +34,16 @@ class ViewController: UIViewController {
 	
 		
 		if let calcMethod = sender.currentTitle {
-			if calcMethod == "+/-" {
-				displayValue = displayValue * -1
-			} else if calcMethod == "AC" {
-				displayLabel.text = "0"
-			} else if calcMethod == "%" {
-				displayValue = displayValue * 0.01
+			
+			let calculator = CalculatorLogic(number: displayValue)
+			
+			guard let result = calculator.calculate(symbol: calcMethod) else {
+				fatalError("The result of the calculation is nil.")
 			}
+			displayValue = result
+			
 		}
+
     
     }
 
