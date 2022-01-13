0efa7938d5ce1c9c395948a4319671a2767743ab
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2022-01-13 16:01:52 -0300

    Refactor with Computed Properties

--- Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
@@ -13,24 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
 	private var isFinishedTypingNumber : Bool = true
+	
+	private var displayValue: Double {
+		get {
+			guard let number = Double(displayLabel.text!) else {
+				fatalError("Cannot convert display label text to a Double.")
+			}
+			return number
+		}
+		set {
+			(displayLabel.text!) = String(newValue)
+		}
+	}
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
 		
 		isFinishedTypingNumber = true
-		
-		guard let number = Double(displayLabel.text!) else {
-			fatalError("Cannot convert display label text to a Double.")
-		}
+	
 		
 		if let calcMethod = sender.currentTitle {
 			if calcMethod == "+/-" {
-				displayLabel.text = String(number * -1)
+				displayValue = displayValue * -1
 			} else if calcMethod == "AC" {
 				displayLabel.text = "0"
 			} else if calcMethod == "%" {
-				displayLabel.text = String(number * 0.01)
+				displayValue = displayValue * 0.01
 			}
 		}
     
@@ -49,11 +58,7 @@ class ViewController: UIViewController {
 				
 				if numValue == "." {
 					
-					guard let currentDisplayValue = Double(displayLabel.text!) else {
-						fatalError("Cannot convert display label text to a Double")
-					}
-					
-					let isInt = floor(currentDisplayValue) == currentDisplayValue
+					let isInt = floor(displayValue) == displayValue
 					
 					if !isInt {
 						return
