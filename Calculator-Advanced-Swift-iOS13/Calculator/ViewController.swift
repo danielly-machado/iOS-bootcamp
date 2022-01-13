90057b2beb6062e4f0d11ce34bf317ff84c52d2f
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2022-01-13 15:30:38 -0300

    Add function on the top three buttons

--- Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
@@ -23,6 +23,16 @@ class ViewController: UIViewController {
 		guard let number = Double(displayLabel.text!) else {
 			fatalError("Cannot convert display label text to a Double.")
 		}
+		
+		if let calcMethod = sender.currentTitle {
+			if calcMethod == "+/-" {
+				displayLabel.text = String(number * -1)
+			} else if calcMethod == "AC" {
+				displayLabel.text = "0"
+			} else if calcMethod == "%" {
+				displayLabel.text = String(number * 0.01)
+			}
+		}
     
     }
 
