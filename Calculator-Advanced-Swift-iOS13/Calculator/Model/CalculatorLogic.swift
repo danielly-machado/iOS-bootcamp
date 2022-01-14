e7ea205a2aeab1f9f3f9a1404b001882c1fe23ed
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2022-01-14 10:38:26 -0300

    Inset switch case and finish adjusts

--- Calculator-Advanced-Swift-iOS13/Calculator/Model/CalculatorLogic.swift
+++ Calculator-Advanced-Swift-iOS13/Calculator/Model/CalculatorLogic.swift
@@ -2,7 +2,7 @@
 //  CalculatorLogic.swift
 //  Calculator
 //
-//  Created by Lidiomar Fernando dos Santos Machado on 13/01/22.
+//  Created by Danielly Alves Ribeiro Machado on 13/01/22.
 //  Copyright © 2022 London App Brewery. All rights reserved.
 //
 
@@ -13,26 +13,51 @@ struct CalculatorLogic  {
 	
 	private var number : Double?
 	
+	private var intermediateCalculation: (n1: Double, calcMethod: String)?
+	
 	mutating func setNumber (_ number: Double) {
 		self.number = number
 	}
 	
-	func calculate(symbol: String) -> Double? {
+	mutating func calculate(symbol: String) -> Double? {
 		
 		if let n = number {
-			if symbol == "+/-" {
+			switch symbol {
+			case "+/-":
 				return n * -1
-			} else if symbol == "AC" {
+			case "AC":
 				return 0
-			} else if symbol == "%" {
+			case "%":
 				return n * 0.01
-			} else if symbol == "+" {
-		
-			} else if symbol == "=" {
-				
+			case "=":
+				return performTwoNumCalculation(n2: n)
+			default:
+				intermediateCalculation = (n1: n, calcMethod: symbol)
 			}
 		}
 		return nil
 	}
-
+	
+	private func performTwoNumCalculation(n2: Double) -> Double? {
+		
+		if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+			
+			switch operation {
+			case "+":
+				return n1 + n2
+			case "-":
+				return n1 - n2
+			case "×":
+				return n1 * n2
+			case "÷":
+				return n1 / n2
+			default:
+				fatalError("The operation passed in does not match any of the cases.")
+			}
+		}
+		return nil
+	}
+	
 }
+
+
